c686714e2579f329d16eb1c76029abeeadb6ec58
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2025-01-30 18:53:00 +0530

    updated

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
